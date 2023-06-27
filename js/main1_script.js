c61384a93a692aa74f839b442c692954ae55ef7c
jeongewon <137126410+jeongewon@users.noreply.github.com>
2023-06-28 02:01:57 +0900

    main1 (230627)

--- js/main1_script.js
+++ js/main1_script.js
@@ -1,13 +1,13 @@
-let slideWrapper = document.querySelector('.MovieChart_wrap'),
-slideContainer = document.querySelector('.MovieChart_slide_wrap'),
-slides = document.querySelectorAll('li'),
+let slideContainer = document.querySelector('.MovieChart_wrap'),
+slideWrapper = document.querySelector('.MovieChart_slide_wrap'),
+slides = document.querySelectorAll('.MovieChart_slide_wrap li'),
 slideCount = slides.length,
 slideWidth = 222,
 slideMargin = 40,
 slidesPerView = 5,
 currentIdx = 0,
-prevBtn = slideWrapper.querySelector('.mC_prevBtn'),
-nextBtn = slideWrapper.querySelector('.mC_nextBtn'),
+prevBtn = document.querySelector('.mC_prevBtn'),
+nextBtn = document.querySelector('.mC_nextBtn'),
 video = document.getElementById('eleMental'),
 videoPlayBtn = document.querySelector('.playbtn'),
 videoPauseBtn = document.querySelector('.pausebtn');
@@ -21,20 +21,35 @@ videoPauseBtn.addEventListener('click',()=>{
 });
 
 
+
+// 무비차트 슬라이드
+slides.forEach((slide,idx)=>{
+    slide.style.left = `${idx*(slideWidth+slideMargin)}px`
+})
+
 function moveSlide(num){
-    slideContainer.style.left = -num*(slideWidth+slideMargin)+'px';
+    slideWrapper.style.left = -num*(slideWidth+slideMargin)+'px';
     currentIdx = num;
+    moveSlide(0)
+    /*
+    if(currentIdx === 0){
+        prevBtn.classList.add('disabled');
+    } else{
+        prevBtn.classList.remove('disabled');
+    }
+    */
 }
-moveSlide(0);
+// moveSlide(0);
 
 nextBtn.addEventListener('click',()=>{
-    if(currentIdx < slideCount-slidesPerView){    
-        moveSlide(currentIdx +5);
+    if(currentIdx < slideCount-slidesPerView){
+      moveSlide(currentIdx+5);
     }
-});
+  });
+  preBtn.addEventListener('click',()=>{
+    if(currentIdx > 0){
+      moveSlide(currentIdx-5);
+    }
+  });
 
-prevBtn.addEventListener('click',()=>{
-    if(currentIdx > 0){    
-        moveSlide(currentIdx -5);
-    }
-});
+
