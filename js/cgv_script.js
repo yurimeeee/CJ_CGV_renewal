8b3024909fd5506934548922ce361d844f9a4ec0
jeongewon <2wonyee@gmail.com>
2023-06-28 17:10:24 +0900

    main1 (230628)

--- js/cgv_script.js
+++ js/cgv_script.js
@@ -1,3 +1,7 @@
+/* HEADER 시작 (유림) */
+ /* HEADER 끝 (유림) */
+
+/* MAIN_1 시작 (이원) */
 let slideContainer = document.querySelector('.MovieChart_wrap'),
 slideWrapper = document.querySelector('.MovieChart_slide_wrap'),
 slides = document.querySelectorAll('.MovieChart_slide_wrap li'),
@@ -6,8 +10,8 @@ slideWidth = 222,
 slideMargin = 40,
 slidesPerView = 5,
 currentIdx = 0,
-prevBtn = document.querySelector('.mC_prevBtn'),
-nextBtn = document.querySelector('.mC_nextBtn'),
+prevBtn = document.querySelector('#prev'),
+nextBtn = document.querySelector('#next'),
 video = document.getElementById('eleMental'),
 videoPlayBtn = document.querySelector('.playbtn'),
 videoPauseBtn = document.querySelector('.pausebtn');
@@ -20,36 +24,34 @@ videoPauseBtn.addEventListener('click',()=>{
     video.pause();
 });
 
-
-
-// 무비차트 슬라이드
-slides.forEach((slide,idx)=>{
-    slide.style.left = `${idx*(slideWidth+slideMargin)}px`
-})
+slideWrapper.style.width = slideCount*(slideWidth+slideMargin)+'px';
 
 function moveSlide(num){
     slideWrapper.style.left = -num*(slideWidth+slideMargin)+'px';
     currentIdx = num;
-    moveSlide(0)
-    /*
+
     if(currentIdx === 0){
         prevBtn.classList.add('disabled');
     } else{
         prevBtn.classList.remove('disabled');
     }
-    */
 }
-// moveSlide(0);
+moveSlide(0)
 
 nextBtn.addEventListener('click',()=>{
     if(currentIdx < slideCount-slidesPerView){
       moveSlide(currentIdx+5);
     }
   });
-  preBtn.addEventListener('click',()=>{
+  prevBtn.addEventListener('click',()=>{
     if(currentIdx > 0){
       moveSlide(currentIdx-5);
     }
   });
+/* MAIN_1 끝 (이원) */
 
+/* MAIN_2 시작 (수연) */
+/* MAIN_2 끝 (수연) */
 
+/* MAIN_3 시작 (정석) */
+/* MAIN_3 끝 (정석) */
